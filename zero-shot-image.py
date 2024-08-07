66dd51f90b875b409294b53f7747b9176e653580
Aya <kent831217@gmail.com>
2024-08-07 16:11:49 +0800

    feat: clip sample

--- zero-shot-image.py
+++ zero-shot-image.py
@@ -1,3 +1,20 @@
-import torch
-from PIL import Image
-import clip
+from transformers import pipeline
+
+# More models in the model hub.
+# model_name = "openai/clip-vit-large-patch14-336"
+model_name = "openai/clip-vit-base-patch16"
+
+classifier = pipeline("zero-shot-image-classification", model=model_name)
+
+image_to_classify = "./asset/cat.jpg"
+labels_for_classification = ["cat", "dog", "lion", "rabbit",
+                             "cat or dog",
+                             "lion and cheetah",
+                             "rabbit and lion"]
+
+# labels_for_classification = ["cat", "dog", "lion", "rabbit"]
+
+scores = classifier(image_to_classify,
+                    candidate_labels=labels_for_classification)
+
+print(scores)
